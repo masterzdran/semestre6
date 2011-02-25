7478bd4f33ddd88b9fdea93fc359512514c31709
nuno.cancelo@gmail.com <nuno.cancelo@gmail.com@eccd3a0e-46b0-bed2-5a8e-bbef10be0016>
2011-02-25 13:42:34 +0000

    Implementing SPI

---
+++ trunk/SE2/workplace/code/src/device/SPI/SPI.c
@@ -0,0 +1,24 @@
+/**
+#=======================================================================
+# SE1   - Sistemas Embebidos 1
+#-----------------------------------------------------------------------
+# Turma:	LI51N
+# Semestre:	Inverno 2010/2011
+# Data:		Novembro/2010
+#-----------------------------------------------------------------------
+# Nome: 	Nuno Cancelo
+# Numero:	31401
+#-----------------------------------------------------------------------
+# Nome:		Nuno Sousa
+# Numero:	33595
+#-----------------------------------------------------------------------
+# LEIC  - Licenciatura em Engenharia Informática e Computadores
+# DEETC - Dep. de Eng. Electrónica e Telecomunicações e Computadores
+# ISEL  - Instituto Superior de Engenharia de Lisboa
+#=======================================================================
+**/ 
+#include "SPI.h"
+
+void SPI_init();
+void SPI_write();
+void SPI_read();
--- trunk/SE2/workplace/code/src/device/SPI/SPI.h
+++ trunk/SE2/workplace/code/src/device/SPI/SPI.h
@@ -2,12 +2,50 @@
 #define SPI_H
 #include "TYPES.h"
 #include "LPC21XX.h"
+
+
+
+
+
+
+#define __SPCR_MASK__     0xF8
+#define __SPCR_RESERVED__ 0x07
+#define __SPCR_CPHA__     0x08
+#define __SPCR_CPOL__     0x10
+#define __SPCR_MSTR__     0x20
+#define __SPCR_LSBF__     0x40
+#define __SPCR_SPIE__     0x80
+
+#define __SPSR_MASK__     0xF8
+#define __SPSR_RESERVED__ 0x07
+#define __SPSR_ABRT__     0x08
+#define __SPSR_MODF__     0x10
+#define __SPSR_ROVR__     0x20
+#define __SPSR_WCOL__     0x40
+#define __SPSR_SPIF__     0x80
+
+#define __SPDR_DATA__     0xFF
+#define __SPCCR_COUNTER__ 0xFF
+
+#define __SPINT_INTERRUPT__ 0x01
+#define __SPINT_RESERVED__  0xFE
+
+
 typedef struct _SPI{
-    U32		CONTROL;
-    U32		STATUS;
-    U32		DATA;
-    U32		CLOCK_COUNTER;
+    U8		CONTROL;
+    U8    DUMMY01[3];
+    U8		STATUS;
+    U8    DUMMY02[3];
+    U8		DATA;
+    U8    DUMMY03[3];
+    U8		CLOCK_COUNTER;
+    U8    DUMMY04[3];
 }LPC_SPI,pLPC_SPI;
+
+typedef struct _SPI_INT{
+    U8    SPINT;
+    U8    DUMMY01[3];
+}LPC_SPI_INT,pLPC_SPI_INT;
 
 
 #endif
