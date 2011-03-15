a1f9da50674c7c02011f739d0e7282d283a8d868
nuno.cancelo@gmail.com <nuno.cancelo@gmail.com@eccd3a0e-46b0-bed2-5a8e-bbef10be0016>
2011-03-15 12:58:32 +0000

    SE2: UART AVE: DOCS

--- trunk/SE2/workplace/code/src/device/UARTS/UART.h
+++ trunk/SE2/workplace/code/src/device/UARTS/UART.h
@@ -5,40 +5,40 @@
 
 
 typedef struct _UART{
-  union{
-    U8    RBR;               //0xE000 C000
+  union{                  /*Addr: 0xE000 C000*/
+    U8    RBR;
     U8    THR;
     U8    DLL;
-    U32   DUMMY01;           //Alinhamento a 32 bits
+    U32   DUMMY01;        /* 32 bit alignment */
   };
-  union{
-    U8    DLM;              //0xE000 C004
+  union{                  /*Addr: 0xE000 C004*/
+    U8    DLM;
     U8    IER;
-    U16   DUMMY02;          //Alinhamento a 32 bits
+    U32   DUMMY02;        /* 32 bit alignment */
   };
-  union{
-    U8    IIR;              //0xE000 C008
+  union{                  /*Addr: 0xE000 C008*/
+    U8    IIR;
     U8    FCR;
-    U32   DUMMY03;          //Alinhamento a 32 bits
+    U32   DUMMY03;        /* 32 bit alignment */
   };
-  U8      LCR ;             //0xE000 C00C
-  U8      DUMMY05;
-  U16     DUMMY06;
-  U32     DUMMY07;
-  U8      LSR;              //0xE000 C014
-  U8      DUMMY08;
-  U16     DUMMY09;
-  U8      SCR;              //0xE000 C01C
-  U8      DUMMY10;
-  U16     DUMMY11;
-  U16     ACR;              //0xE000 C020
-  U16     DUMMY13;
-  U32     DUMMY14;
-  U32     FDR;              //0xE000 C028
-  U32     DUMMY15;
-  U8      TER;              //0xE000 C030
-  U8      DUMMY16;
-  U16     DUMMY17;
+  U8      LCR ;           /*Addr: 0xE000 C00C*/
+  U8      DUMMY04[3];
+  U8      MCR;            /*Addr: 0xE000 C010*/ /*Avaiable in UART1*/
+  U8      DUMMY05[3];          
+  U8      LSR;            /*Addr: 0xE000 C014*/
+  U8      DUMMY06[3];
+  U8      MSR;            /*Addr: 0xE000 C018*/ /*Avaiable in UART1*/
+  U8      DUMMY07[3];
+  U8      SCR;            /*Addr: 0xE000 C01C*/
+  U8      DUMMY08[3];
+  U16     ACR;            /*Addr: 0xE000 C020*/
+  U8      DUMMY09[2];
+  U32     DUMMY10;        /*Addr: 0xE000 C024*/
+  U8      FDR;            /*Addr: 0xE000 C028*/
+  U8      DUMMY11[3];     
+  U32     DUMMY12;        /*Addr: 0xE000 C02C*/
+  U8      TER;            /*Addr: 0xE000 C030*/
+  U8      DUMMY13[3];
 }LPC_UART,*pLPC_UART;
 
 
