f0d674b1a7b8886b8a270731b4b1849f9a8bf8e8
nuno.cancelo@gmail.com <nuno.cancelo@gmail.com@eccd3a0e-46b0-bed2-5a8e-bbef10be0016>
2011-03-21 15:17:05 +0000

    Re-pathring

--- trunk/SI2/Pratica/view.php
+++
@@ -1,4 +0,0 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Frameset//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd">
-<html dir="ltr">
-<head><meta http-equiv="content-type" content="text/html; charset=UTF-8" /><title>SI2-LI51N1011SV: 1ª Aula Prática</title></head>
-<frameset rows="130,*"><frame src="view.php?id=32919&amp;type=file&amp;frameset=top" /><frame src="http://moodle.isel.pt/deetc/file.php/1500/1a_Aula_Pratica.pdf" /></frameset></html>
