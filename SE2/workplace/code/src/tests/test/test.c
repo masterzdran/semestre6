73342bdd17dc6cf6f6ba5e88a75c98e8b87e43c8
ng.sousa@gmail.com <ng.sousa@gmail.com@eccd3a0e-46b0-bed2-5a8e-bbef10be0016>
2011-03-23 12:10:58 +0000

    SE2: lpc a comunicar com o enc

--- SE2/workplace/code/src/tests/test/test.c
+++ SE2/workplace/code/src/tests/test/test.c
@@ -16,13 +16,13 @@ int main(){
   SPI_Device spis[1]= {
     {
       0,
-      10,   /*clock divider */
-      1<<8,    /*id do periferico = GPIO pin 8*/
-      8,    /*num bits palavra*/
+      10,   		/*clock divider */
+      1<<8,    		/*id do periferico = GPIO pin 8*/
+      8,    		/*num bits palavra*/
       SPI_PRIOR_TO_FIRST_SCK_RISING_EDGE,    /*mode*/
       SPI_MASTER,    /*role = master*/
-      0,    /*not started yet*/
-      SPI_MSB     /*byte shitf ??*/
+      0,    		/*not started yet*/
+      SPI_MSB     	/*byte shitf ??*/
     }
   };
   TIMER_init(pTIMER1,58982400/MICRO);
@@ -43,12 +43,15 @@ int main(){
  
   while(1){
     U8 out_buffer[] = {0x35, 0x7a};
+	U8 out_buffer2[] = {0x1E , 0xFF}; /*enviar 0x1E ncomo primeiro byte e depois dummy para receber o 0x80*/
     U8 in_buffer[sizeof(out_buffer)];
     SPI_start_device(&spis[0]);
     SPI_transfer(&spis[0], sizeof(out_buffer), out_buffer, in_buffer);
+	gpio_clear(0x100);	/*activar RST do ENC*/
+	SPI_transfer(&spis[0], sizeof(out_buffer2), out_buffer2, in_buffer);
     SPI_stop_device(&spis[0]);
 	
-  
+
   
   
   }
