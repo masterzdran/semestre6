322966089cdddf98cbbbbaacddff15cbb84b245a
nuno.cancelo@gmail.com <nuno.cancelo@gmail.com@eccd3a0e-46b0-bed2-5a8e-bbef10be0016>
2011-04-06 10:25:05 +0000

    ENC28J60

--- SE2/workplace/code/src/peripherical/ENC28J60/ENC28J60.h
+++ SE2/workplace/code/src/peripherical/ENC28J60/ENC28J60.h
@@ -397,9 +397,9 @@ void ENC_system_reset_command();
 
 
 
-#define ENC_write_reg16(A, B, C)  	ENC_write_reg((A), (B),(C),true)
-#define ENC_write_reg8(A, B, C)  	ENC_write_reg((A), (B),(C),false)
-#define ENC_read_reg8(A, B) 		ENC_read_reg((A), (B),false)
-#define ENC_read_reg16(A, B) 		ENC_read_reg((A), (B),true)
+#define ENC_WRITE_REG16(A, B, C)  	ENC_write_reg((A), (B),(C),true)
+#define ENC_WRITE_REG8(A, B, C)  	ENC_write_reg((A), (B),(C),false)
+#define ENC_READ_REG8(A, B) 		ENC_read_reg((A), (B),false)
+#define ENC_READ_REG16(A, B) 		ENC_read_reg((A), (B),true)
 #endif
 
--- SE2/workplace/code/src/peripherical/ENC28J60/Ethernet.c
+++ SE2/workplace/code/src/peripherical/ENC28J60/Ethernet.c
@@ -1,13 +1,14 @@
 #include "Ethernet.h"
 #include "ENC28J60.h"
 #include "TYPES.h"
+
 #define SINGLE_BYTE	1
 #define DOUBLE_BYTE 2
 #define TSV_SIZE 7
 #define RSV_SIZE 4
 #define TRANSMITE_LATE_COLLITION_BUG(A)		((A)[3] & (1 << 5))	
 #define MAX_TX_RETRIES				10
-#define PROMISCUOUS_MODE 0
+#define PROMISCUOUS_MODE 0      /*ERXFCON_REGISTER Enumerator shoul be used to filter packets*/
 #define FULL_DUPLEX  0x15
 #define HALF_DUPLEX  0x12
 #define  MAIPGH_HALF_DUPLEX	0x0C12
@@ -28,6 +29,10 @@ static inline void Ethernet_start_tx(){
 	ENC_bit_field_clear(EIR,EIR_TXERIF|EIR_TXIF);
 	ENC_bit_field_set(ECON1,ECON1_TXRTS);	
 }
+
+
+static pETHERNET_Device ethDevice;
+
 U8 Ethernet_send(U8* packet, U16 packet_size){
 	U8 tsv[TSV_SIZE],aux;
 	U8 retry = 0;
@@ -43,12 +48,15 @@ U8 Ethernet_send(U8* packet, U16 packet_size){
 	do{
 		Ethernet_start_tx();
 		do{
-			aux = ENC_read_reg(EIR,BANK00,false);
+			/*aux = ENC_read_reg(EIR,BANK00,false);*/
+            aux = ENC_READ_REG8(EIR,BANK00);
 		}while((aux & EIR_TXIF) == 0);
-		
-		ENC_write_reg(B0_ERDPTL,BANK00,__ETHERNET_TX_START_PTR__ + packet_size + 1,true);
+        
+		ENC_WRITE_REG16(B0_ERDPTL,BANK00,__ETHERNET_TX_START_PTR__ + packet_size + 1);
+		/*ENC_write_reg(B0_ERDPTL,BANK00,__ETHERNET_TX_START_PTR__ + packet_size + 1,true);*/
 		ENC_read_buffer_memory(tsv,TSV_SIZE);
-		aux =  ENC_read_reg(EIR,BANK00,false);
+		/*aux =  ENC_read_reg(EIR,BANK00,false);*/
+        aux =  ENC_READ_REG8(EIR,BANK00);
 	}while((aux & EIR_TXERIF) && TRANSMITE_LATE_COLLITION_BUG(tsv) && (retry++ < MAX_TX_RETRIES) )	;
 	return ETHERNET_OK;
 }
@@ -83,29 +91,91 @@ U32 Ethernet_receive(U8* buffer, U32 buffer_size,U32* read_size){
 	return ETHERNET_OK;
 }
 
-U8 Ethernet_init(U8* mac){
-	if (mac == 0)
-		return ETHERNET_NULL_POINTER;
+/**
+ * @brief 
+ * */
+U8 Ethernet_init(pETHERNET_Device ethernetDevice){
+	if (&(ethernetDevice->mac) == 0)
+		return ETHERNET_MAC_NULL_POINTER;
 	
+    ethDevice = ethernetDevice;
+    /* Definition of Enc28j60 spi default values */
+    ethDevice->ethernetDevice.mode       = SPI_PRIOR_TO_FIRST_SCK_RISING_EDGE;
+    ethDevice->ethernetDevice.role       = SPI_MASTER;
+    ethDevice->ethernetDevice.nbrbits    = 8;
+    ethDevice->ethernetDevice.byteShift  = SPI_MSB;
+    ethDevice->ethernetDevice.started    = 0;
+    
+    /*Intialization of spi device of ethernet*/
+    if ( SPI_init(&(ethDevice->ethernetDevice),1) != SPI_SUCESS ){
+        return ETHERNET_SPI_INIT_ERROR;
+    } 
+    
+    /*Reset Enc28j60 to default values*/
+    ENC_system_reset_command();
+    
+    /*6.1 Receive Buffer*/
+    /*
 	ENC_write_reg(B0_ERXSTL,BANK00,__ETHERNET_RX_START_PTR__,true);
 	ENC_write_reg(B0_ERXNDL,BANK00,__ETHERNET_RX_END_PTR__,true);
 	ENC_write_reg(B0_ERXRDPTL,BANK00,__ETHERNET_RX_END_PTR__,true);
-	
+	*/
+	ENC_WRITE_REG16(B0_ERXSTL,BANK00,__ETHERNET_RX_START_PTR__);
+	ENC_WRITE_REG16(B0_ERXNDL,BANK00,__ETHERNET_RX_END_PTR__);
+	ENC_WRITE_REG16(B0_ERXRDPTL,BANK00,__ETHERNET_RX_END_PTR__);
+    
+    
+    /*
 	ENC_write_reg(B1_ERXFCON,BANK01,PROMISCUOUS_MODE,false);
-	ENC_write_reg(B3_MAADR1,BANK03,mac[0],false);
-	ENC_write_reg(B3_MAADR2,BANK03,mac[1],false);
-	ENC_write_reg(B3_MAADR3,BANK03,mac[2],false);
-	ENC_write_reg(B3_MAADR4,BANK03,mac[3],false);
-	ENC_write_reg(B3_MAADR5,BANK03,mac[4],false);
-	ENC_write_reg(B3_MAADR6,BANK03,mac[5],false);
+	ENC_write_reg(B3_MAADR1,BANK03,ethernetDevice->mac[0],false);
+	ENC_write_reg(B3_MAADR2,BANK03,ethernetDevice->mac[1],false);
+	ENC_write_reg(B3_MAADR3,BANK03,ethernetDevice->mac[2],false);
+	ENC_write_reg(B3_MAADR4,BANK03,ethernetDevice->mac[3],false);
+	ENC_write_reg(B3_MAADR5,BANK03,ethernetDevice->mac[4],false);
+	ENC_write_reg(B3_MAADR6,BANK03,ethernetDevice->mac[5],false);
 	
 	ENC_write_reg(B2_MACON1,BANK02,MACON1_MARXEN,false);
 	ENC_write_reg(B2_MACON3,BANK02,MACON3_FRMLNEN + MACON3_TXCRCEN + MACON3_PADCFG0,false);
 	ENC_write_reg(B2_MACON4,BANK02,MACON4_DEFER,false);
-	ENC_write_reg(B2_MAMXFLL,BANK02,MAX_FRAME_LEN,true);
-	ENC_write_reg(B2_MABBIPG,BANK02,FULL_DUPLEX,false);
-	ENC_write_reg(B2_MAIPGL,BANK02,MAIPGH_FULL_DUPLEX,true);
-	ENC_write_reg(ECON1,BANK02,ECON1_RXEN,false);
+    */
+    /*6.3 Receive Filters*/
+    ENC_WRITE_REG8(B1_ERXFCON,BANK01,PROMISCUOUS_MODE);
+    
+    /*6.5 MAC Initialization Settings*/
+	ENC_WRITE_REG8(B3_MAADR1,BANK03,ethernetDevice->mac[0]);
+	ENC_WRITE_REG8(B3_MAADR2,BANK03,ethernetDevice->mac[1]);
+	ENC_WRITE_REG8(B3_MAADR3,BANK03,ethernetDevice->mac[2]);
+	ENC_WRITE_REG8(B3_MAADR4,BANK03,ethernetDevice->mac[3]);
+	ENC_WRITE_REG8(B3_MAADR5,BANK03,ethernetDevice->mac[4]);
+	ENC_WRITE_REG8(B3_MAADR6,BANK03,ethernetDevice->mac[5]);
+	
+	ENC_WRITE_REG8(B2_MACON1,BANK02,MACON1_MARXEN);
+	ENC_WRITE_REG8(B2_MACON3,BANK02,MACON3_FRMLNEN + MACON3_TXCRCEN + MACON3_PADCFG0);
+	ENC_WRITE_REG8(B2_MACON4,BANK02,MACON4_DEFER);
+    
+	/*ENC_write_reg(B2_MAMXFLL,BANK02,MAX_FRAME_LEN,true);*/
+    ENC_WRITE_REG16(B2_MAMXFLL,BANK02,MAX_FRAME_LEN);
+    /**
+     * @todo
+     * Config Full Duplex     
+     * *//*
+	if ( ethernetDevice->duplex == ETHERNET_FULL_DUPLEX){
+        ENC_write_reg(B2_MABBIPG,BANK02,FULL_DUPLEX,false);
+        ENC_write_reg(B2_MAIPGL,BANK02,MAIPGH_FULL_DUPLEX,true);
+	}else{
+        ENC_write_reg(B2_MABBIPG,BANK02,HALF_DUPLEX,false);
+        ENC_write_reg(B2_MAIPGL,BANK02,MAIPGH_HALF_DUPLEX,true);
+    }
+    ENC_write_reg(ECON1,BANK02,ECON1_RXEN,false);	
+    */
+    if ( ethernetDevice->duplex == ETHERNET_FULL_DUPLEX){
+        ENC_WRITE_REG8(B2_MABBIPG,BANK02,FULL_DUPLEX);
+        ENC_WRITE_REG16(B2_MAIPGL,BANK02,MAIPGH_FULL_DUPLEX);
+	}else{
+        ENC_WRITE_REG8(B2_MABBIPG,BANK02,HALF_DUPLEX);
+        ENC_WRITE_REG16(B2_MAIPGL,BANK02,MAIPGH_HALF_DUPLEX);
+    }
+    ENC_WRITE_REG8(ECON1,BANK02,ECON1_RXEN);
 	return ETHERNET_OK;
 }
 
