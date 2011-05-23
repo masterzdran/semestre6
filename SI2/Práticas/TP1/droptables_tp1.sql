9c6ae7508986983d1a6bc4f23aff2604648ddf3e
claudia.alexandra.carvalho@gmail.com <claudia.alexandra.carvalho@gmail.com@eccd3a0e-46b0-bed2-5a8e-bbef10be0016>
2011-05-23 16:38:51 +0000

    Ordem de droptable e inversa aos create's

--- SI2/Práticas/TP1/droptables_tp1.sql
+++ SI2/Práticas/TP1/droptables_tp1.sql
@@ -1,6 +1,63 @@
 use SI2_TP1;
 
+if exists(select * 
+		from INFORMATION_SCHEMA.TABLES
+		where TABLE_NAME = 'FRIENDS')
+drop table FRIENDS;
 
+if exists(select * 
+		from INFORMATION_SCHEMA.TABLES
+		where TABLE_NAME = 'MENU_COURSES')
+drop table MENU_COURSES;
+
+if exists(select * 
+		from INFORMATION_SCHEMA.TABLES
+		where TABLE_NAME = 'COURSES_INGREDIENTS')
+drop table COURSES_INGREDIENTS;
+
+if exists(select * 
+		from INFORMATION_SCHEMA.TABLES
+		where TABLE_NAME = 'ORDERS_LOG')
+drop table ORDERS_LOG;
+
+if exists(select * 
+		from INFORMATION_SCHEMA.TABLES
+		where TABLE_NAME = 'ORDERS')
+drop table ORDERS;
+
+if exists(select * 
+		from INFORMATION_SCHEMA.TABLES
+		where TABLE_NAME = 'PURCHASES')
+drop table PURCHASES;
+if exists(select * 
+		from INFORMATION_SCHEMA.TABLES
+		where TABLE_NAME = 'SUPPLIERS')
+drop table SUPPLIERS;
+
+if exists(select * 
+		from INFORMATION_SCHEMA.TABLES
+		where TABLE_NAME = 'INGREDIENTS')
+drop table INGREDIENTS;
+
+if exists(select * 
+		from INFORMATION_SCHEMA.TABLES
+		where TABLE_NAME = 'EVENT')
+drop table EVENT;
+
+if exists(select * 
+		from INFORMATION_SCHEMA.TABLES
+		where TABLE_NAME = 'MENU')
+drop table MENU;
+
+if exists(select * 
+		from INFORMATION_SCHEMA.TABLES
+		where TABLE_NAME = 'BOOKING')
+drop table BOOKING;
+
+if exists(select * 
+		from INFORMATION_SCHEMA.TABLES
+		where TABLE_NAME = 'REGISTERED')
+drop table REGISTERED;
 
 if exists(select * 
 		from INFORMATION_SCHEMA.TABLES
@@ -12,74 +69,10 @@ if exists(select *
 		where TABLE_NAME = 'COMMENTS')
 drop table COMMENTS;
 
-
 if exists(select * 
 		from INFORMATION_SCHEMA.TABLES
 		where TABLE_NAME = 'COURSES')
 drop table COURSES;	
-
-if exists(select * 
-		from INFORMATION_SCHEMA.TABLES
-		where TABLE_NAME = 'REGISTERED')
-drop table REGISTERED;
-
-
-if exists(select * 
-		from INFORMATION_SCHEMA.TABLES
-		where TABLE_NAME = 'EVENT')
-drop table EVENT;
-
-if exists(select * 
-		from INFORMATION_SCHEMA.TABLES
-		where TABLE_NAME = 'COURSES_INGREDIENTS')
-drop table COURSES_INGREDIENTS;
-
-if exists(select * 
-		from INFORMATION_SCHEMA.TABLES
-		where TABLE_NAME = 'MENU_COURSES')
-drop table MENU_COURSES;
-
-if exists(select * 
-		from INFORMATION_SCHEMA.TABLES
-		where TABLE_NAME = 'MENU')
-drop table MENU;
-
-if exists(select * 
-		from INFORMATION_SCHEMA.TABLES
-		where TABLE_NAME = 'PURCHASES')
-drop table PURCHASES;
-
-if exists(select * 
-		from INFORMATION_SCHEMA.TABLES
-		where TABLE_NAME = 'ORDERS')
-drop table ORDERS;
-
-if exists(select * 
-		from INFORMATION_SCHEMA.TABLES
-		where TABLE_NAME = 'ORDERS_LOG')
-drop table ORDERS_LOG;
-
-if exists(select * 
-		from INFORMATION_SCHEMA.TABLES
-		where TABLE_NAME = 'SUPPLIERS')
-drop table SUPPLIERS;
-
-if exists(select * 
-		from INFORMATION_SCHEMA.TABLES
-		where TABLE_NAME = 'INGREDIENTS')
-drop table INGREDIENTS;
-
-
-if exists(select * 
-		from INFORMATION_SCHEMA.TABLES
-		where TABLE_NAME = 'BOOKING')
-drop table BOOKING;
-
-
-if exists(select * 
-		from INFORMATION_SCHEMA.TABLES
-		where TABLE_NAME = 'FRIENDS')
-drop table FRIENDS;
 
 if exists(select * 
 		from INFORMATION_SCHEMA.TABLES
@@ -88,13 +81,13 @@ drop table CUSTOMER;
 
 if exists(select * 
 		from INFORMATION_SCHEMA.TABLES
-		where TABLE_NAME = 'COUNTRY')
-drop table COUNTRY;
+		where TABLE_NAME = 'ADDRESS')
+drop table ADDRESS;
 
 if exists(select * 
 		from INFORMATION_SCHEMA.TABLES
-		where TABLE_NAME = 'ADDRESS')
-drop table ADDRESS;
+		where TABLE_NAME = 'COUNTRY')
+drop table COUNTRY;
 
 --USE [master]
 --GO
