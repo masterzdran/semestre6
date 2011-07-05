01560f118f1d004854a59acc502365d59c05f973
claudia.alexandra.carvalho@gmail.com <claudia.alexandra.carvalho@gmail.com@eccd3a0e-46b0-bed2-5a8e-bbef10be0016>
2011-07-05 20:47:44 +0000

    Falta: Actualizar EA, COLOCAR TRIGGER PARA ALERTAR QUE INGREDIENTES ESTAO FORA DE PRAZO (1º Parte)!! Criar um Catálogo único e associá-lo a cada prato!! Transferência de Stock: Mudar o stock para outro restaurante e Encomendar o stock que ficou em falta.

---
+++ SI2/Práticas/TP2/DataBase/Ex4-f.sql
@@ -0,0 +1,58 @@
+use SI2_TP1;
+
+/* Ex4 - f)
+Produzir a lista de ingredientes em falta com a indicação dos respectivos fornecedores. 
+Se um ingrediente tiver mais do que um fornecedr deve ser indicado o que, em média, vende com 
+preço mais baixo
+*/
+if OBJECT_ID('SuggestOrder') IS NOT NULL
+	drop function SuggestOrder;
+go	
+
+create function SuggestOrder()
+returns @suggestionTemp table(
+			ID			int,
+			SUPPLIER_ID int,
+			QTY			decimal(10,3))
+as
+	begin
+		declare @stock decimal(10,3), @reserved decimal(10,3), @ingredientID int
+		
+		--get cursor with current stock
+		declare ingredients cursor
+		for select INGREDIENTS.ID, INGREDIENTS.QTY_RESERVED
+				from INGREDIENTS 
+		open ingredients
+		fetch next from ingredients into @ingredientID, @reserved		
+		while(@@FETCH_STATUS=0)
+			begin
+			select @stock = sum(STOCK) from LOT where LOT.INGREDIENTS_ID=@ingredientID
+			if @stock is null
+				set @stock=0
+			--check each ingredient: if stock-reserved is lower than minimum qty then
+			--verify wich supplier is cheapeast and add to suggested order
+			if @stock < (select MIN_QTY from INGREDIENTS where ID=@ingredientID)
+				begin
+				declare @supplierID int
+				set @supplierID=0
+				select @supplierID=temp.ID from
+					(select TOP 1 SUPPLIER_ID as ID, AVG(PRICE) as av
+						from LOT where INGREDIENTS_ID=@ingredientID
+						group by SUPPLIER_ID
+						order by 2) as temp
+				insert into @suggestionTemp values (@ingredientID, @supplierID, 
+					(select ORDER_QTY from INGREDIENTS where ID=@ingredientID))
+				end			
+			fetch next from ingredients into @ingredientID, @reserved
+			end
+		close ingredients
+		deallocate ingredients
+	return
+	end
+	
+	
+
+
+
+
+
---
+++ SI2/Práticas/TP2/DataBase/Ex4-k.sql
@@ -0,0 +1,23 @@
+use SI2_TP1
+/*Ex4 - k)
+	Apresentar os cardápios que dão mais lucro.
+*/
+if OBJECT_ID('MostProfitMenu') IS NOT NULL
+	drop function MostProfitMenu;
+go	
+
+create function MostProfitMenu()
+returns @MostSales table(
+			NAME_MENU			varchar(30),
+			PROFIT			smallmoney)
+as
+	begin
+		insert into @MostSales
+				select MENU.NAME, SUM(SALE_PRICE - COST_PRICE) as DIF
+	from BOOKING inner join NORMAL_BOOKING on BOOKING.ID = NORMAL_BOOKING.BOOKING_ID
+				 inner join MENU on NORMAL_BOOKING.MENU_ID = MENU.ID
+	GROUP BY MENU.NAME
+	ORDER BY DIF desc
+		
+	return
+	end
