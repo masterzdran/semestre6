e73edf15b68773f8f08280933ee51d144df1459f
nuno.cancelo@gmail.com <nuno.cancelo@gmail.com@eccd3a0e-46b0-bed2-5a8e-bbef10be0016>
2011-08-27 19:13:41 +0000

    AVE

---
+++ AVE/Praticas/ChelasUIMaker/ChelasUIMaker.Engine/ChelasUIView.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ChelasUIMaker.Engine
+{
+    class ChelasUIView : View , XView
+    {
+
+        public ChelasUIView(Config config)
+        {
+
+        }
+
+        
+        public override System.Windows.Forms.Control this[string s]
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
