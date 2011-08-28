a99b07c2e39fd3d2edffbdf3c1011e4740ad8713
nuno.cancelo@gmail.com <nuno.cancelo@gmail.com@eccd3a0e-46b0-bed2-5a8e-bbef10be0016>
2011-08-28 14:29:44 +0000

    AVE

--- AVE/Praticas/ChelasUIMaker/ChelasUIMaker.Engine/ChelasUIArea.cs
+++ AVE/Praticas/ChelasUIMaker/ChelasUIMaker.Engine/ChelasUIArea.cs
@@ -7,19 +7,62 @@ using System.Windows.Forms;
 
 namespace ChelasUIMaker.Engine
 {
+    class ChelasUIArea<T> : IArea<T> where T : new()
+    {
+        //private Delegate _action;
+        public Control _control;
+        //Corrected
+        public ChelasUIArea(Action<T> action) // reordenado o código
+        {
+            T t = new T();
+            // if (t == null) return; // é sempre falso pq tens ali uma restriçao
+            if (action != null) action(t); // pode ou nao ser nulo. só aplicas a acçao uma vez, nao tens necessidade de a guardar
+
+            //action(t);
+            _control = t as Control;
+            //_action = action;
+        }
+        public IArea<T> WithController<C>()
+        {
+            //TODO: Validar o tipo;
+            Config._controller = typeof(C);
+            return this;
+        }
+
+        public IArea<T> WithContent<U>(IArea<U> content)
+        {
+            if (content != null)
+            {
+                var t = content.GetType().GetField("_control").GetValue(content) as Control;
+                //var t = content as Control;
+                if (_control != null && t != null)
+                {
+                    _control.Controls.Add(t);
+                }
+
+            }
+            //Não Gosto -> tambem nao, mas a outra soluçao muda-te muita coisa. tens de ter aqui uma XView e fazes _todo o código aqui dentro quase (pq tb te obriga a usar a ChelasUIView aqui)
+            Config._control = _control;
+            return this;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("Name: ${0}", this.GetType().FullName);
+        }
+    }
+    /*
     class ChelasUIArea<T> : IArea<T> where T : new() 
     {
-        private Delegate _action;
         public Control _control;
 
         public ChelasUIArea(Action<T> action)
         {
-            if (action == null) return;
+            
             T t = new T();
-            if (t == null) return;
-            action(t);
+            if (action != null) action(t);
             _control = t as Control;
-            _action = action;
+
         }
         public IArea<T> WithController<C>()
         {
@@ -49,4 +92,5 @@ namespace ChelasUIMaker.Engine
             return String.Format("Name: ${0}",this.GetType().FullName);
         }
     }
+     * */
 }
