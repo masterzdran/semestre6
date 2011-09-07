80e72828877c17f02439ce361339da6b56dc94f4
nuno.cancelo@gmail.com <nuno.cancelo@gmail.com@eccd3a0e-46b0-bed2-5a8e-bbef10be0016>
2011-09-07 17:46:22 +0000

    AVE

--- AVE/Praticas/ChelasUIMaker/ChelasUIMaker.Engine/Config.cs
+++ AVE/Praticas/ChelasUIMaker/ChelasUIMaker.Engine/Config.cs
@@ -6,54 +6,29 @@
 
     public abstract class Config 
     {
-        readonly String _dataAssembly = "System.Windows.Forms,version=4.0.0.0,Culture=neutral,PublicKeyToken=b77a5c561934e089";
-        Assembly _assembly;
-        Type[] _types;
-        //Não Gosto
-        public static Control _control;
-        public static Type _controller;
+        Object _area;
+        Type _parameter;
 
         public void Load()
         {
-            try
-            {
-                if (_assembly == null)
-                {
-                    _assembly = Assembly.Load(_dataAssembly);
-                    _types = _assembly.GetTypes();
-                }
-           }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-            }
-            _control = new Control();
-            _control.SuspendLayout();
             LoadConfig();
-            _control.ResumeLayout();
         }
 
         protected abstract void LoadConfig();
-        protected IArea<T> DefineArea<T>(Action<T> a) 
-            where T : new()
+        
+        protected IArea<T> DefineArea<T>(Action<T> a) where T : new()
         {
-            IArea<T> t = ((hasType<T>()) ? new ChelasUIArea<T>(a) : null);
-            //T tmp = new T();
-            //_control = tmp as Control;
-            return t;
+            IArea<T> area = new ChelasUIArea<T>(a);
+
+            _area = area;
+            _parameter = typeof(T);
+            return area;
         }
 
-        public Boolean hasType<T>()
-        {
-            Type tf = typeof(T);
-            foreach (Type t in _types)
-            {
-                if( t.Name.Equals(tf.Name))
-                        return true;
-            }
-            return false;
-        }
-   
+        public Object IArea { get { return _area; } }
+        public Type ParameterType { get { return _parameter; } }
+
+
     }
 
 }
--- AVE/Praticas/ChelasUIMaker/ChelasUIMaker.Engine/IArea.cs
+++ AVE/Praticas/ChelasUIMaker/ChelasUIMaker.Engine/IArea.cs
@@ -1,9 +1,12 @@
 ﻿namespace ChelasUIMaker.Engine
 {
+
     public interface IArea<T>
     {
         IArea<T> WithController<C>();
 
         IArea<T> WithContent<U>(IArea<U> content);
+        System.Type Controller();
+        System.Windows.Forms.Control Control();
     }
 }
