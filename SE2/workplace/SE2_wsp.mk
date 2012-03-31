185e9e71a8af00d252a9368b39adf747269bdbd7
nuno.cancelo@gmail.com <nuno.cancelo@gmail.com@eccd3a0e-46b0-bed2-5a8e-bbef10be0016>
2012-03-31 08:18:02 +0000

    clean

--- SE2/workplace/SE2_wsp.mk
+++
@@ -1,8 +0,0 @@
-.PHONY: clean All
-
-All:
-	@echo ----------Building project:[ workplace - Debug ]----------
-	@"$(MAKE)" -f "workplace.mk"
-clean:
-	@echo ----------Cleaning project:[ workplace - Debug ]----------
-	@"$(MAKE)" -f "workplace.mk" clean
--- SE2/workplace/eCos/3.0/eCos.ISEL.SE2.31401.build_build/language/c/libc/i18n/v3_0/src/ctype.o.d
+++
@@ -1,4 +0,0 @@
-src/ctype.o.d:  \
- /home/masterzdran/Applications/eCos/3.0/ecos-3.0/packages/language/c/libc/i18n/v3_0/src/ctype.cxx \
-  /home/masterzdran/WorkingArea/Isel/semestre6/SE2/workplace/eCos/3.0/eCos.ISEL.SE2.31401.build_install/include/pkgconf/libc_i18n.h \
-  /home/masterzdran/WorkingArea/Isel/semestre6/SE2/workplace/eCos/3.0/eCos.ISEL.SE2.31401.build_install/include/cyg/libc/i18n/ctype.inl
